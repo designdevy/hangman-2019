48ccf5a31b53dca8aa00ba68ee8798c6d60e6ea1
Muhammad Athar <muhammad.athar@agoda.com>
2019-07-29 07:49:36 +0700

    hangman app setup done

---
+++ src/Components/Alphabet/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Alphabet: React.FC = () => {
+  return (
+    <div>
+      <p>A B C D E F G H I J K L M N O P Q R S T U V W X Y Z</p>
+    </div>
+  )
+}
+
+export default Alphabet
---
+++ src/Components/App/App.tsx
@@ -0,0 +1,13 @@
+import Alphabet from 'Components/Alphabet'
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <p>App</p>
+      <Alphabet />
+    </div>
+  )
+}
+
+export default App
